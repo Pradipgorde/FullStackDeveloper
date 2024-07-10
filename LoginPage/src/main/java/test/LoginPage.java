a97d1e9b52680bd646eb1bcb42f6f0fa789b8513
Pradip Gorde <gordepradip555@gmail.com>
2024-07-10 20:22:36 +0530

    ok

---
+++ LoginPage/src/main/java/test/LoginPage.java
@@ -0,0 +1,49 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+@SuppressWarnings("serial")
+@WebServlet("/log1")
+public class LoginPage extends GenericServlet
+{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException 
+	{
+		System.out.println("Login page");
+		
+		PrintWriter pw = res.getWriter();
+		res.setContentType("text/html");
+		
+		String id = req.getParameter("id");
+		String pass = req.getParameter("pass");
+		
+		if(id.equals("Pradip") && pass.equals("Gorde"))
+				{
+			         pw.println("Login Sucessfully...");
+			          
+			          RequestDispatcher rd = req.getRequestDispatcher("Choice.html");
+			          
+			          rd.include(req, res);
+			          
+			             
+				}else
+				{
+					pw.println("user Id OR Pass-word incorrect...");
+					
+					RequestDispatcher rd = req.getRequestDispatcher("login.html");
+					rd.include(req, res);
+				}
+		
+		
+		
+	}
+
+}
